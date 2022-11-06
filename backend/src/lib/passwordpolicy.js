8970c9176d62f3ad9b9e248724ada05edac4d173
yotermidai <105938618+yotermidai@users.noreply.github.com>
2022-11-06 15:02:00 +0100

    Delete backend directory

--- backend/src/lib/passwordpolicy.js
+++
@@ -1,7 +0,0 @@
-// Check if user's password match password policy
-function strongPassword(password) {
-        var regExp = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/;
-        return regExp.test(password);
-}
-
-exports.strongPassword = strongPassword
